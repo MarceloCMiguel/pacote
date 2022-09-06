227324b4faef06279fc544816ec9ad9100acdee4
MarceloCMiguel <marcelocmiguel@outlook.com>
2022-09-06 11:20:22 -0300

    project

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='dev_aberto_marcelocm8',
+      version='0.1',
+      packages=['dev_aberto'],
+      install_requires=[
+        'requests',
+    ],
+    scripts=['scripts/hello.py'],
+
+
+      )
