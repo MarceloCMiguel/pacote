436d11bf82c9cfadcfb6214086fd92bba7887aaf
MarceloCMiguel <marcelocmiguel@pop-os.localdomain>
2022-09-15 11:52:12 -0300

    feat: internacionalizacao e localizacao

--- script/hello.py
+++ script/hello.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+import os
+from time import strptime
 import dev_aberto
-
-
+from babel.dates import format_date
+from babel.numbers import format_number
+import gettext
+import datetime
+gettext.install('cli', localedir='locale') 
 if __name__ == '__main__':
+    print(os.getenv('LANGUAGE'))
     date, name = dev_aberto.hello()
-    print('Último commit feito em:', date, ' por', name)
+    
+    date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
+    print(_('Last commit made in:'),  format_date( date), _(' by'), name)
---
+++ script/routine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LANGUAGE=pt_BR.utf8
+export LC_NUMERIC=pt_BR.utf8
+export LC_TIME=pt_BR.utf8
+python3 hello.py
+
+export LANGUAGE=en_US.utf8
+export LC_NUMERIC=en_US.UTF-8;
+export LC_TIME=en_US.UTF-8;
+python3 hello.py
+
+
